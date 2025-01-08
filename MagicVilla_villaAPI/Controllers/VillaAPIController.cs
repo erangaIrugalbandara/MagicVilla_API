c8da110fab57e3d9c925d9b640ceaad4f556b852
Eranga <erangaibj@gmail.com>
2025-01-08 22:36:30 +0530

    Get methods added

---
+++ MagicVilla_villaAPI/Controllers/VillaAPIController.cs
@@ -0,0 +1,33 @@
+﻿using MagicVilla_villaAPI.Data;
+using MagicVilla_villaAPI.Models;
+using MagicVilla_villaAPI.Models.Dto;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MagicVilla_villaAPI.Controllers
+{
+    [Route("api/VillaAPI")]
+    [ApiController]
+    public class VillaAPIController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult<IEnumerable<VillaDTO>> GetVillas()
+        {
+            return Ok(VillaStore.villaList);
+        }
+
+        [HttpGet("{id:int}")]
+        public ActionResult<VillaDTO> GetVilla(int id)
+        {
+            if(id == 0)
+            {
+                return BadRequest();   
+            }
+            var villa = VillaStore.villaList.FirstOrDefault(u => u.Id == id);
+            if (villa == null)
+            {
+                return NotFound();
+            } 
+            else return Ok(villa);
+        }
+    }
+}
---
+++ MagicVilla_villaAPI/Models/Villa.cs
@@ -0,0 +1,10 @@
+﻿namespace MagicVilla_villaAPI.Models
+{
+    public class Villa
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public DateTime RecordCreated { get; set; }
+
+    }
+}
